167b2181d9d21135e8c87973985f562ec41f0394
Valentin Bezzubov <hammerviking@yandex.ru>
2019-07-29 21:47:05 +0000

    Repaired desktop manager and respi-config installation.

--- initial.sh
+++ initial.sh
@@ -6,7 +6,7 @@ add-apt-repository ppa:ubuntu-raspi2/ppa ppa:ubuntu-pi-flavour-makers/ppa
 apt-get update
 
 # install one of the desktop managers and tune the X server
-if [[ $0 == "kubuntu" || $0 == "lubuntu" || $0 == "xubuntu" || $0 == "ubuntu" ]]    # TODO: add other desktop managers
+if [[ $0 == "kubuntu" || $0 == "lubuntu" || $0 == "xubuntu" || $0 == "ubuntu" ]]; then    # TODO: add other desktop managers
     apt-get install -y $0-desktop 
 
     # Install VideoCore utilities
@@ -22,7 +22,13 @@ if [[ $0 == "kubuntu" || $0 == "lubuntu" || $0 == "xubuntu" || $0 == "ubuntu" ]]
 fi
 
 # install raspi-config from Raspberry archive
-wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20190424_all.deb && apt-get install lua5.1 triggerhappy whiptail parted -y && dpkg -i raspi-config_20190424_all.deb	# TODO: installation of the newest version without hardcode
+apt-get install libnewt0.52 whiptail parted triggerhappy lua5.1 alsa-utils -y
+apt-get install -fy
+wget http://archive.raspberrypi.org/debian/pool/main/r/raspi-config/raspi-config_20190424_all.deb &&  dpkg -i raspi-config_20190424_all.deb	# TODO: installation of the newest version without hardcode
+rm raspi-config*.deb
+
+#Mount booting partition to make raspi-config work
+mount /dev/mmcblk0p1 /boot
 
 # Disable wait-online service on startup
 systemctl disable systemd-networkd-wait-online.service
