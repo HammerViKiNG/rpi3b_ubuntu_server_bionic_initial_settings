a202d40b300a764a4ec3c6db02f9b7004063fa8b
Valentin Bezzubov <hammerviking@yandex.ru>
2019-07-30 14:23:18 +0300

    Handled multiple PPA adding.

--- initial.sh
+++ initial.sh
@@ -2,18 +2,26 @@
 
 
 # Add Raspberry Pi repositories and update the package index
-add-apt-repository -y ppa:ubuntu-raspi2/ppa
-apt-get update
 
-add-apt-repository -y ppa:ubuntu-pi-flavour-makers/ppa
+if ! grep -q "^deb .*ubuntu-raspi2/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*;
+then
+    add-apt-repository -y ppa:ubuntu-raspi2/ppa
+fi
+
+if ! grep -q "^deb .*ubuntu-pi-flavour-makers/ppa" /etc/apt/sources.list /etc/apt/sources.list.d/*;
+then
+    add-apt-repository -y ppa:ubuntu-pi-flavour-makers/ppa
+fi
+
 apt-get update
 
 # Install VideoCore utilities
 apt-get install -y libraspberrypi-bin libraspberrypi-bin-nonfree
 
 # install one of the desktop managers and tune the X server
-if [[ $1 == "kubuntu-desptop" || $1 == "lubuntu-desktop" || $1 == "xubuntu-desktop" || $1 == "ubuntu-desktop" || $1 == "lxde" || $1 == "lxqt"]]; then    # TODO: add other desktop managers
-    apt-get install -y $1  xserver-xorg-video-fbturbo
+if [[ $1 == "kubuntu-desptop" || $1 == "lubuntu-desktop" || $1 == "xubuntu-desktop" || $1 == "ubuntu-desktop" || $1 == "lxde" || $1 == "lxqt" ]]; 
+then    # TODO: add other desktop managers
+    apt-get install -y $1 xserver-xorg-video-fbturbo
 
     # Copy xorg.conf file to /etc/X11/xorg.conf
     cp xorg.conf /etc/X11/xorg.conf
